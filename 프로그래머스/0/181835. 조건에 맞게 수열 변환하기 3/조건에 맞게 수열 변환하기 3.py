67d697d8f62e56a59611591693a1a529127e6778
seram2002-wq <seram2002@ewhain.net>
2026-09-10 10:15:01 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 26.76 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
@@ -0,0 +1,9 @@
+def solution(arr, k):
+    answer = []
+    if k%2==1:
+        for num in arr:
+            answer.append(num*k)
+    elif k%2==0:
+        for num in arr:
+            answer.append(num+k)
+    return answer
